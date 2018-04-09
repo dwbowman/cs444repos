8ba92b723db972731ffb0425ea7bf0aabe2252ad
n644f787 <n644f787@riker.cs.wichita.edu>
2018-04-09 12:42:21 -0500

    this is final test

--- newfile1.cpp
+++ newfile1.cpp
@@ -1,4 +1,5 @@
 //in newfile1 THis is a change to push to the fourth commit
 //second commit in file
 // this is a fifth commit to add
+// another testing commit
 // this is the third and final commit to test
